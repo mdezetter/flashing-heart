4cb97978f2b5341defc0efd991a922bab1114d20
Michelle <36121618+mdezetter@users.noreply.github.com>
2023-09-21 21:23:03 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+basic.show_icon(IconNames.HEART)
+basic.clear_screen()
+basic.pause(500)
+basic.show_icon(IconNames.SMALL_HEART)
+basic.clear_screen()
+basic.pause(500)
---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+basic.pause(500)
+basic.showIcon(IconNames.SmallHeart)
+basic.clearScreen()
+basic.pause(500)
